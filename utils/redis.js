1e8f877774c4053dbfa2a51ab6f2aa669737d713
Joseph Eiba <josepheiba@icloud.com>
2024-09-30 19:44:50 +0100

    ok

--- utils/redis.js
+++ utils/redis.js
@@ -20,10 +20,19 @@ class RedisClient {
     this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
   isAlive() {
     return this.client.connected;
   }
 
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
+   */
   async get(key) {
     try {
       return await this.getAsync(key);
@@ -33,6 +42,13 @@ class RedisClient {
     }
   }
 
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     try {
       await this.setAsync(key, value, "EX", duration);
@@ -41,6 +57,11 @@ class RedisClient {
     }
   }
 
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
   async del(key) {
     try {
       await this.delAsync(key);
