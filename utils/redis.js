358161563e4852ad1a74dc56a6055f865ec83276
Joseph Eiba <josepheiba@icloud.com>
2024-09-30 19:27:02 +0100

    ok

--- utils/redis.js
+++ utils/redis.js
@@ -4,9 +4,15 @@ import { promisify } from "util";
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+
     this.client.on("error", (error) => {
       console.error(`Redis client error: ${error}`);
     });
+
+    // Promisify Redis commands
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
