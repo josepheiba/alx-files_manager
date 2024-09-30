23d6953f3d96c76d39c59bc3dec8184a6a703c50
Joseph Eiba <josepheiba@icloud.com>
2024-09-30 21:01:23 +0100

    ok1

---
+++ utils/db.js
@@ -0,0 +1,44 @@
+import { mongoClient } from "mongodb";
+
+/**
+ * Class for performing operations with Mongo service
+ */
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || "localhost";
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || "files_manager";
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect((err) => {
+      if (err) {
+        console.error("MongoDB connection error:", err);
+      } else {
+        this.db = this.client.db(database);
+        console.log("Connected to MongoDB");
+      }
+    });
+  }
+
+  isAlive() {
+    return (
+      !!this.client &&
+      !!this.client.topology &&
+      this.client.topology.isConnected()
+    );
+  }
+
+  async nbUsers() {
+    if (!this.db) return 0;
+    return this.db.collection("users").countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) return 0;
+    return this.db.collection("files").countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
