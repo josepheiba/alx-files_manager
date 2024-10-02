1d7a11253d00eaf0d30234776ac568f7117e1a27
Joseph Eiba <josepheiba@icloud.com>
2024-10-02 20:51:57 +0100

    ok

---
+++ controllers/UsersController.js
@@ -0,0 +1,39 @@
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const existingUser = await usersCollection.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const result = await usersCollection.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    const newUser = {
+      id: result.insertedId.toString(),
+      email,
+    };
+
+    return res.status(201).json(newUser);
+  }
+}
+
+export default UsersController;
