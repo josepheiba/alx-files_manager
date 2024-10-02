632c9aee5def56bccfaf10c2a6f1cda6ccf6d3f7
Joseph Eiba <josepheiba@icloud.com>
2024-10-02 21:06:16 +0100

    ok

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-named-as-default */
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -33,6 +34,26 @@ class UsersController {
     };
 
     return res.status(201).json(newUser);
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id.toString(), email: user.email });
   }
 }
 
