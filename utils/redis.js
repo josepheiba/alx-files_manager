6d200f277ed59427d86d77557c00fcd801658809
Joseph Eiba <josepheiba@icloud.com>
2024-10-02 20:34:22 +0100

    ok1

--- utils/redis.js
+++ utils/redis.js
@@ -1,12 +1,12 @@
-import redis from 'redis';
-import { promisify } from 'util';
+import redis from "redis";
+import { promisify } from "util";
 
 /**
- * Represents a Redis client.
+ * Redis client.
  */
 class RedisClient {
   /**
-   * Creates a new RedisClient instance.
+   * RedisClient instance.
    */
   constructor() {
     this.client = redis.createClient();
@@ -14,13 +14,13 @@ class RedisClient {
     this.setAsync = promisify(this.client.set).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
 
-    this.client.on('error', (error) => {
+    this.client.on("error", (error) => {
       console.error(`Redis client error: ${error}`);
     });
   }
 
   /**
-   * Checks if this client's connection to the Redis server is active.
+   * Checks client's connection with the Redis server.
    * @returns {boolean}
    */
   isAlive() {
@@ -28,8 +28,8 @@ class RedisClient {
   }
 
   /**
-   * Retrieves the value of a given key.
-   * @param {String} key The key of the item to retrieve.
+   * gets value of the key.
+   * @param {String} key is not here bruh.
    * @returns {String | Object}
    */
   async get(key) {
@@ -42,23 +42,23 @@ class RedisClient {
   }
 
   /**
-   * Stores a key and its value along with an expiration time.
-   * @param {String} key The key of the item to store.
-   * @param {String | Number | Boolean} value The item to store.
-   * @param {Number} duration The expiration time of the item in seconds.
+   * Sets a key-value pair in Redis with an optional expiration.
+   * @param {String} key The Redis key to set.
+   * @param {String | Number | Boolean} value The value to associate with the key.
+   * @param {Number} duration The expiration time in seconds (optional).
    * @returns {Promise<void>}
    */
   async set(key, value, duration) {
     try {
-      await this.setAsync(key, value, 'EX', duration);
+      await this.setAsync(key, value, "EX", duration);
     } catch (error) {
       console.error(`Error setting key ${key}: ${error}`);
     }
   }
 
   /**
-   * Removes the value of a given key.
-   * @param {String} key The key of the item to remove.
+   * Deletes a key from Redis.
+   * @param {String} key The Redis key to delete.
    * @returns {Promise<void>}
    */
   async del(key) {
